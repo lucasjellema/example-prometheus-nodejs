9f858e8cd14a85946463cc213e7b0909733e58f3
Lucas Jellema <lucasjellema@gmail.com>
2018-09-20 10:58:45 +0200

    Changing default response message

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res, next) => {
   setTimeout(() => {
-    res.json({ message: 'Hello World!' })
+    res.json({ message: 'Hello World from Sample App!' })
     next()
   }, Math.round(Math.random() * 200))
 })
